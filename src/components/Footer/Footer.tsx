a0f80241ae22e0aa32ca724b03c779c2a8d4dd2c
Pnunes <pno.mco.jcn@gmail.com>
2024-01-03 23:51:00 -0300

    finish footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,15 +1,26 @@
-import { Repeat, Shuffle, SkipBack, SkipForward, Play } from "lucide-react";
+import {
+    Repeat,
+    Shuffle,
+    SkipBack,
+    SkipForward,
+    Play,
+    Mic2,
+    LayoutList,
+    Laptop2,
+    Volume,
+    Maximize2,
+} from "lucide-react";
 
 export const Footer = () => {
     return (
-        <footer className="bg-zinc-800 border-zinc-700 p-6 flex items-center justify-between">
+        <footer className="bg-zinc-950 border-zinc-700 p-6 flex items-center justify-between rounded-lg">
             <div className="flex items-center gap-3 justify-between w-full">
                 <div className="flex gap-4">
                     <img src="/lastMusic.png" alt="" width={45} height={45} />
                     <strong className="font-normal">
                         Flow Natalino (feat. Aklipe44, LinoMC & TcheloBeats)
                         <br />
-                        <span className="text-xs text-zinc-700">
+                        <span className="text-xs text-zinc-600">
                             Lil Chainz, Lil Fuub, MHRAP, Aklipe44 & LinoMC
                         </span>
                     </strong>
@@ -35,6 +46,19 @@ export const Footer = () => {
                         </div>
                         <span className="text-xs text-zinc-400">5:11</span>
                     </div>
+                </div>
+
+                <div className="flex items-center gap-2">
+                    <Mic2 size={20} />
+                    <LayoutList size={20} />
+                    <Laptop2 size={20} />
+                    <div className="flex items-center gap-2">
+                        <Volume size={20} />
+                        <div className="h-1 rounded-full w-24 bg-zinc-600">
+                            <div className="w-10 rounded-full h-1 bg-zinc-200"></div>
+                        </div>
+                    </div>
+                    <Maximize2 size={20} />
                 </div>
             </div>
         </footer>
